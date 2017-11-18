be26df0015d6387e2cc24147e8209df7aedb4a18
TianleZhang <cqtianle@163.com>
2017-11-18 12:07:34 +0800

    添加用户特征

---
+++ tl/src/userFeature.py
@@ -0,0 +1,27 @@
+import configparser
+import pandas as pd
+
+import datetime as dt
+from dateutil.parser import parse
+
+
+conf = configparser.ConfigParser()
+conf.read("./jd.conf")
+
+root_dir = conf.get("local", "root_dir_local")
+feature_url = conf.get("local", "feature_url")
+
+user = pd.DataFrame(pd.read_csv(root_dir + 't_user.csv'))
+
+# pd.to_datetime(user['active_date'])
+
+# 计算激活日期与2016年11月1日的时间差
+user['delta_time'] = user.apply(lambda x: (dt.datetime(2016, 11, 1) - parse(x['active_date'])).days,  axis=1)
+
+# 将age和sex转为onehot编码，并删除原来列
+user_m = pd.get_dummies(user, columns=['age', 'sex'])
+user_m = user_m.drop('active_date', axis=1)
+
+# 保存用户特征
+user_m.to_csv(feature_url + 'foo.csv')
+
---
+++ tl/src/util.py
@@ -0,0 +1,26 @@
+from dateutil.parser import parse
+
+
+def split_by_month(data):
+    return parse(data).month
+
+
+def count_price_per_order(column):
+    return column["price"] * column["qty"] - column["discount"]
+
+
+def get_pay_per_month(column):
+    return column["loan_amount"] / column["plannum"]
+
+
+def get_remain_loan(column, month):
+    tmp = column["loan_amount"] - column["pay_per_month"] * (month - column["month"])
+    if tmp >= 0:
+        return tmp
+    return 0
+
+
+def get_remain_pay(column, month):
+    if month - column["month"] <= column["plannum"] and month - column["month"] > 0:
+        return column["pay_per_month"]
+    return 0
